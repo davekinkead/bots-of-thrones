e572874b74b883f0e204cbb6669eb8ca3edac2b5
Dave Kinkead <dave@kinkead.com.au>
2016-05-17 13:14:29 +1000

    Add message listener

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var Slackbot = require('slackbots')
-var express = require('express')
-var http = require('http').Server(express);
+var express = require('express')()
+var http = require('http').Server(express)
 
-//  we need express to stop Heroku crashing on 404
-express.set('port', (process.env.PORT || 5000));
+var Ygritte = {
+  params: {
+    as_user: false,
+    icon_url: 'https://raw.githubusercontent.com/davekinkead/davekinkead.github.io/master/media/ygritte.jpg'
+  }
+}
 
-express.on('/', function(req, res) {
-  res.send('<h1>Winter is coming</h1>')
-})
-
-express.listen(express.get('port'), function() {
-  console.log('Node app is running on port', express.get('port'))
-})
+var JohnSnow = {
+  params: {
+    as_user: false,
+    username: 'John Snow',
+    icon_url: 'https://avatars3.githubusercontent.com/u/611877?v=3&s=460'
+  }
+}
 
 //  Slackbot
 var bot = new Slackbot({
@@ -20,20 +24,30 @@ var bot = new Slackbot({
 })
 
 bot.on('start', function() {
-  var params = {
-    as_user: false,
-    icon_url: 'https://raw.githubusercontent.com/davekinkead/davekinkead.github.io/master/media/ygritte.jpg'
-  }
 
-  bot.postMessage('#sandpit', 'Winter is coming...', params)
-
-  bot.on('message', function(data) {
-      // all ingoing events https://api.slack.com/rtm
-      console.log(data);
-  })
+  bot.postMessage('#sandpit', 'Winter is coming...', Ygritte.params)
+    .fail(function(data) {
+      console.log('Failed to connect somehow...')
+      console.log(data)
+    })
 })
 
 bot.on('message', function(data) {
-    // all ingoing events https://api.slack.com/rtm
-    console.log(data);
+  console.log(data);
+
+  if (data.type === 'message' && data.subtype !== 'bot_message') {
+    bot.postMessage('#sandpit', 'You said..' + data.text, JohnSnow.params)
+  }
+})
+
+
+//  we need express to stop Heroku crashing on 404
+express.set('port', process.env.PORT || 5000)
+
+express.get('/', function(req, res) {
+  res.send('<h1>Winter is coming...</h1>')
+})
+
+express.listen(express.get('port'), function() {
+  console.log('Node app is running on port', express.get('port'))
 })
